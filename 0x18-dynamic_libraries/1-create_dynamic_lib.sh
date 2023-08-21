a292ce5d6913c67f499e67a8391405f67817c229
rmanwa <robamanwa@gmail.com>
2023-08-21 16:09:54 -0700

    fixes

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c -fPIC *.c
-gcc -shared -o liball.so *.o
-export LD-LIBRARY_PATH=.:$LD_LIBRARY_PATH
+gcc -fPIC -g -c -Wall -pedantic -Werror -Wextra *.c
+gcc -shared -Wl,-soname,liball.so -o liball.so *.o -lc
